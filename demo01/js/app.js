f732c69c05dfe9cc367779aa30bc4335e4dff6f6
Petar Petrov <petar.petrov@de.gbs.com>
2014-06-12 16:39:00 +0200

    Add dynamic update of persons list

--- demo01/js/app.js
+++ demo01/js/app.js
@@ -3,7 +3,21 @@ var myApp = angular.module('myApp', []);
 myApp
     .controller('main', function($scope) {
         $scope.vorname = '';
-        $scope.show = function() {
-            alert($scope.vorname);
+        $scope.nachname = '';
+
+        $scope.Persons = [];
+
+        $scope.show = function(name) {
+            alert($scope.vorname || name);
+        };
+        $scope.show2 = function(name) {
+            alert(name);
+        };
+        $scope.add = function(vorname, nachname) {
+            $scope.Persons.push({
+                id: $scope.Persons.length + 1,
+                vorname: vorname,
+                nachname: nachname
+            });
         };
     });
