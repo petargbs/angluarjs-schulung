47b9aea2dd768c6629f32c183f34cfb77cc10640
Petar Petrov <petar.petrov@de.gbs.com>
2014-06-13 11:47:35 +0200

    Modified server rules

--- demo02/server.js
+++ demo02/server.js
@@ -8,7 +8,11 @@ server.use(restify.CORS({
     origins: ['*'],
     credentials: true
 }));    
+
 server.use(restify.fullResponse());
+
+server.use(restify.pre.sanitizePath());
+
 server.opts(/\.*/, function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, HEAD, OPTION');
@@ -17,21 +21,22 @@ server.opts(/\.*/, function (req, res, next) {
     return next();
 });
 
-// server.get('/offline/cache.manifest', function(req,res,next){
-//     var filePath = path.join(__dirname,'www/offline/cache.manifest');
-//     fs.readFile(filePath, function(error, data){
-//         res.header('Content-Type', 'text/cache-manifest');
-//         res.send(200, data.toString());
-//     });
-// });
-// server.get(/offline\/.*/, restify.serveStatic({
-//     directory: path.join(__dirname, 'www'),
-//     maxAge:0
-// }));
-
 server.listen(8080, function(){
     console.log("Service listen on: 8080");
 });
+
+server.get('/offline/cache.manifest', function(req,res,next){
+    var filePath = path.join(__dirname,'www/offline/cache.manifest');
+    fs.readFile(filePath, function(error, data){
+        res.header('Content-Type', 'text/cache-manifest');
+        res.send(200, data.toString());
+    });
+});
+server.get(/offline(\/)*.*/, restify.serveStatic({
+    directory: path.join(__dirname, 'www'),
+    maxAge:0,
+    default: 'index.html'
+}));
 
 server.get(/\/.*/, restify.serveStatic({
     directory: path.join(__dirname, 'www/person'),
@@ -39,15 +44,15 @@ server.get(/\/.*/, restify.serveStatic({
     default: 'index.html'
 }));
 
-// server.get(/person\/.*/, restify.serveStatic({
-//     directory: path.join(__dirname, 'www'),
-//     maxAge:0
-// }));
+server.get(/person\/.*/, restify.serveStatic({
+    directory: path.join(__dirname, 'www'),
+    maxAge:0
+}));
 
-// server.get('/persons', function(req, res, next){
-//     res.send(200, []);
-// });
+server.get('/persons', function(req, res, next){
+    res.send(200, []);
+});
 
-// server.get('/demodata', function(req, res, next){
-//     res.send(200, []);
-// });
+server.get('/demodata', function(req, res, next){
+    res.send(200, []);
+});
